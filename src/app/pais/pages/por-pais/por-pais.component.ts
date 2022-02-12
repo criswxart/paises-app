8dae1ad2ce92d7d9ee265d1def5e4ade3611269d
Cristian Godoy <criswxart@gmail.com>
2022-02-11 21:38:57 -0300

    creacion de componentes
    se realizó la creación de componentes y el consumo de las apis

---
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/paises.interface';
+
+@Component({
+  selector: 'app-por-pais',
+  templateUrl: './por-pais.component.html',
+  styleUrls: ['./por-pais.component.scss']
+})
+export class PorPaisComponent  {
+
+  termino : string    = '';
+  hayError: boolean   = false;
+  paises  : Country[] = [];
+
+  constructor( private paisService:PaisService ){}
+
+  buscar(){
+    this.hayError = false;
+    console.log(this.termino);
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe( (paises) => {
+
+        this.paises = paises;
+        console.log(this.paises);
+      }, (err) => {
+        this.hayError = true;
+        this.paises   =  [];
+        console.log(err);
+      });
+
+  }
+}
