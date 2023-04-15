1a78fdc476bbb7274cf7a11a8ea1cb5680de676d
Cristian <criswxart@gmail.com>
2023-04-15 14:10:13 -0400

    se cambia de version bootstrap y se agregan nuevas rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,34 +4,32 @@ import { PorCapitalComponent } from "./pais/pages/por-capital/por-capital.compon
 import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pais/pages/por-region/por-region.component';
 import { VerPaisComponent } from './pais/pages/ver-pais/ver-pais.component';
-
-
+import { HomePageComponent } from "./shared/pages/home-page/home-page.component";
+import { ContactPageComponent } from "./shared/pages/contact-page/contact-page.component";
+import { AboutPageComponent } from "./shared/pages/about-page/about-page.component";
 
 const routes: Routes = [
     {
         path: '',
-        component: PorPaisComponent,
-        pathMatch: 'full'
+        component: HomePageComponent,
     },
     {
-        path: 'region',
-        component: PorRegionComponent
+        path: 'contact',
+        component: ContactPageComponent,
     },
     {
-        path: 'capital',
-        component: PorCapitalComponent
+        path: 'about',
+        component: AboutPageComponent,
     },
     {
-        path: 'pais/:id',
-        component: VerPaisComponent
+        path: 'paises',
+        loadChildren: () => import('./pais/pais.module').then( m => m.PaisModule)
     },
     {
         path:'**',
         redirectTo: ''
     }
 ]
-
-
 
 @NgModule({
     imports:[
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { FooterComponent } from './footer/footer.component';
-import { MenuComponent } from './menu/menu.component';
-
+import { AboutPageComponent } from './pages/about-page/about-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    SidebarComponent,
-    FooterComponent,
-    MenuComponent
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
+    SidebarComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
+    AboutPageComponent,
+    ContactPageComponent,
+    HomePageComponent,
     SidebarComponent
   ]
 })
